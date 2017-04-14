8eff395653cc0ad8253a789bd1abe0ca222879a3
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2017-04-14 17:08:05 +0200

    Sightly improved version of chat

--- client.erl
+++ client.erl
@@ -1,7 +1,34 @@
 -module(client).
--export([connect/2]).
+-export([start/0, loop/1, connect/2, process_message/2]).
 
 connect(Addr, Port) ->
-	{ok, Address} = inet:parse_address(Addr),
-	{ok, Socket} = gen_tcp:connect(Address, Port, [binary, {active,true}]),
-	gen_tcp:send(Socket, "Hey there first shell!").
+	case inet:parse_address(Addr) of
+		{error, X} ->
+			io:format("Error ~p parsing address ~p~n", [X, Addr]);	
+		{ok, Address} ->
+		case gen_tcp:connect(Address, Port, [binary, {active,true}]) of
+			{ok, Socket} ->
+				{ok, Socket};
+			{error, X} ->
+				io:format("Error on connection to ~p on port ~p for reason ~p~n", [Addr, Port, X]),
+				{error, X}
+		end
+	end.
+
+loop(Socket) ->
+	process_message(Socket, io:get_line("Chat>")),
+	loop(Socket).
+
+start() ->
+	{Status, Conn} = connect("127.0.0.1", 8888),	
+	case Status of
+		ok -> 
+				loop(Conn);
+		error ->
+			"Terminating program..."
+	end.
+
+process_message(Socket, Message) ->
+	gen_tcp:send(Socket, Message),
+	Message.
+
--- server.erl
+++ server.erl
@@ -1,9 +1,13 @@
 -module(server).
--export([listen_to_connections/1]).
+-export([listen_to_connections/1, recv_loop/1]).
 
 listen_to_connections(Port) ->
 	{ok, ListenSocket} = gen_tcp:listen(Port, [{active, false}, binary]),
 	{ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
-	{ok, Bin} = gen_tcp:recv(AcceptSocket, 0),
-    ok = gen_tcp:close(AcceptSocket),
-    Bin.
+	recv_loop(AcceptSocket),
+    ok = gen_tcp:close(AcceptSocket).
+
+recv_loop(Socket) ->
+    {ok, Data} = gen_tcp:recv(Socket, 0),
+    io:format("Message: ~p~n",[Data]),
+    recv_loop(Socket).
